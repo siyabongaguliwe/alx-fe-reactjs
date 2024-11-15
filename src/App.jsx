312c473c8c887f1c2c501e4e49823d4f2095ff77
siyabongaguliwe <siyabongaguliwe1@gmail.com>
2024-11-15 14:59:28 +0200

    Add my react app

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import WelcomeMessage from './components/WelcomeMessage.jsx';
+import './App.css';
+
+function App() {
+    return (
+        <div>
+            <WelcomeMessage />
+        </div>
+    );
+}
+
+export default App;
