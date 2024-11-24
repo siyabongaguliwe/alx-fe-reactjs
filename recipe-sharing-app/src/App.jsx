217448721f8fa2bf6b10cf95d341c61cf0af004b
siyabongaguliwe <siyabongaguliwe1@gmail.com>
2024-11-24 22:05:09 +0200

    Updated CSS to use Forced Colors Mode standard

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import RecipeList from './components/RecipeList';
+import AddRecipeForm from './components/AddRecipeForm';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Recipe Sharing Application</h1>
+      <AddRecipeForm />
+      <RecipeList />
+    </div>
+  );
+}
+
+export default App;
