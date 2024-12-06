e3868da85ed3bb6107eb43a09e2c7dad827d290a
siyabongaguliwe <siyabongaguliwe1@gmail.com>
2024-12-06 09:06:45 +0200

    Add new Tailwind React integration project

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
+import UserProfile from './components/UserProfile'; // Import the UserProfile component
 
 function App() {
   const [count, setCount] = useState(0);
@@ -31,6 +32,7 @@ function App() {
       <p className="read-the-docs text-center mt-4">
         Click on the Vite and React logos to learn more
       </p>
+      <UserProfile /> {/* Include the UserProfile component */}
     </>
   );
 }
