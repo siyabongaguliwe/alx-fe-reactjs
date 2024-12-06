51a69e3b067efcff92a5debb9cf4742918afceb6
siyabongaguliwe <siyabongaguliwe1@gmail.com>
2024-12-06 10:06:08 +0200

    Initial setup of Recipe Sharing Platform with Tailwind CSS

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,9 +1,11 @@
-module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // You can set it to 'media' or 'class' if needed
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+    './public/index.html',
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
-
+}
