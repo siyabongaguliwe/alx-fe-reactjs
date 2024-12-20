0ce25f37b24a45882043d491228629b191a5154a
siyabongaguliwe <siyabongaguliwe1@gmail.com>
2024-12-20 16:17:57 +0200

    Improve UI and add advanced search capabilities to GitHub User Search Application

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,11 +1,12 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://api.github.com/users';
+const BASE_URL = 'https://api.github.com/search/users';
 const API_KEY = process.env.REACT_APP_GITHUB_API_KEY;
 
-export const fetchUserData = async (username) => {
+export const fetchUserData = async (username, location, minRepos) => {
   try {
-    const response = await axios.get(`${BASE_URL}/${username}`, {
+    const query = `q=${username}+location:${location}+repos:>${minRepos}`;
+    const response = await axios.get(`${BASE_URL}?${query}`, {
       headers: {
         Authorization: `token ${API_KEY}`
       }
@@ -15,7 +16,7 @@ export const fetchUserData = async (username) => {
     if (error.response && error.response.status === 404) {
       throw new Error('Looks like we can\'t find the user');
     } else {
-      throw new Error('Looks like we can\'t find the user');
+      throw new Error('An error occurred while fetching user data');
     }
   }
 };
