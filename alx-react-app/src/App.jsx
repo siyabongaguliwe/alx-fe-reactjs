ed6b98a00a0f9022b72c0f11bc764f703f02c30d
siyabongaguliwe <siyabongaguliwe1@gmail.com>
2025-01-02 19:30:08 +0200

    Initial commit: Set up React app with Vite and added UserProfile component

---
+++ alx-react-app/src/App.jsx
@@ -0,0 +1,13 @@
+// src/App.jsx
+import React from 'react';
+import UserProfile from './components/UserProfile';
+
+const App = () => {
+  return (
+    <div>
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+    </div>
+  );
+};
+
+export default App;
